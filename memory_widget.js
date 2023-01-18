de0c224a74c013fd8238028490e873edfe6174eb
robin thomas <robin.or.thomas@gmail.com>
2023-01-18 16:42:41 +0100

    béta version; ajout de commentaire et configuration du widget elementor plus clair

--- memory_widget.js
+++ memory_widget.js
@@ -1,48 +1,65 @@
+//le js est chargé au chargement de la page.
 window.onload=function(){
+    //count compte le nombre de cartes retourné en même temps (max:2) et winCount le nombre d'images trouvés
+    //document est utilisé pour lire les propriétés de l'html formatté par le php (lire l'html pour mieux comprendre)
     let count = 0;
     let winCount=0;  
     var core=document.getElementById("memorygame");
     let win=core.getAttribute("data-win");
     let face=core.getAttribute("data-face");
     var buttons=document.getElementsByClassName("memory-button");
+    //nombre d'images a retourner pour gagner;
     let winEnds=buttons.length;
-    console.log(winEnds);
+    //rafraîchissement de la page si le joueur rafraîchit la page.
     var winButton=document.getElementById("replay");
     winButton.addEventListener('click',event=>{
         window.location.reload();
     });
+    //on tourne une fonction a chaque clic sur chaque image pour la retourner
     for (var btn of buttons){
         btn.addEventListener('click', event => {
-            console.log(count);
+            //on vérifie que le joueur peut retourner les cartes avant de faire quoi que ce soit
             if (event.target.getAttribute('data-isShowed') == "false" && count < 2) {
-                console.log("e:"+event.target.getAttribute('data-realImage'));
+                //l'image réelle remplace l'image retourné
                 event.target.src = event.target.getAttribute('data-realImage');
+                //on change les attributs html pour expliquer que la carte est affiché et si c'est la premiére ou la deuxiéme carte retourné
                 event.target.setAttribute("data-isShowed", "true");
                 event.target.setAttribute('data-counter', count);
+                //on incrémente la variable permettant de compter de carte retourné.
                 count = count + 1;
+                //on regarde si deux cartes sont retournés
                 if (count == 2) {
+                    //on attend un peu de retourner les cartes.
                     setTimeout(() => {
+                        //on remplace l'image par une image de face pour l'image qui a été appuyé par le joueur et l'image selectionné en premier
                         event.target.src = face;
                         var tabData = document.querySelectorAll('[data-counter="0"]');
                         tabData[0].src = face;
+                        //si les deux images selectionnés sont les mêmes, le joueur a gagné
                         if (tabData[0].getAttribute("data-realImage") == event.target.getAttribute("data-realImage")) {
+                            //on change les images des cartes par l'image qu'il faut
                             tabData[0].src = win;
                             event.target.src = win;
+                            //on supprime l'attribut permettant de savoir si la carte est affiché puisque la carte n'a plus a être retourné
                             event.target.removeAttribute("data-IsShowed");
                             tabData[0].removeAttribute('data-IsShowed');
+                            //on augmente de 2 le nombre de carte gagné et on vérifie si c'est les derniéres cartes avant de gagner
                             winCount=winCount+2;
                             if(winCount==winEnds){
                              var display=document.getElementById("win");
+                             //on affiche le message pour montrer aux joueurs qu'il a joué
                              display.style.display="block";
                             }
 
                         }
+                        //si les deux cartes ne sont pas les deux mêmes, on les remets a l'envers
                         else {
                             tabData[0].setAttribute('data-IsShowed', "false");
 
                             event.target.setAttribute("data-IsShowed", "false");
 
                         }
+                        //on remet a zéro le compteur puisque deux cartes ont été retourné.
                         tabData[0].removeAttribute('data-counter');
                         event.target.removeAttribute("data-counter");
                         count = 0;
--- widgets/memory_widget.php
+++ widgets/memory_widget.php
@@ -1,47 +1,54 @@
 <?php
+//fichier de définition du widget de memory
 class Memory_Widget extends \Elementor\Widget_Base {
-
+	//le nom du widget
 	public function get_name() {
 		return 'memory_widget';
 	}
 
+	//titre du widget
 	public function get_title() {
-		return esc_html__( 'memory widget', 'elementor-addon' );
+		return esc_html__( 'memory widget', 'memory_widget' );
 	}
 
+	//icone représentant le widget
 	public function get_icon() {
 		return 'eicon-code';
 	}
 
+	//catégorie du widget
 	public function get_categories() {
-		return [ 'jeux' ];
+		return [ 'basic' ];
 	}
 
+	//mot clés pour le widget
 	public function get_keywords() {
 		return [ 'memory' ];
 	}
 
     
+	//enregistrement des options de configuration du widget
 	protected function register_controls() {
 
 		// Content Tab Start
-
+		//section pour le contenu
 		$this->start_controls_section(
 			'section_title',
 			[
-				'label' => esc_html__( 'Content', 'textdomain' ),
+				'label' => esc_html__( 'Contenu', 'textdomain' ),
 				'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
 			]
 		);
+		//ajout des options des widgets
 		$this->add_control(
 			'list',
 			[
-				'label' => esc_html__( 'List', 'textdomain' ),
+				'label' => esc_html__( 'images pour les cartes', 'textdomain' ),
 				'type' => \Elementor\Controls_Manager::REPEATER,
 				'fields' => [
 				[
 				'name'=>'images',
-				'label' => esc_html__( 'Choose Image', 'textdomain' ),
+				'label' => esc_html__( 'Choississez votre image', 'textdomain' ),
 				'type' => \Elementor\Controls_Manager::MEDIA,
 				'default' => [
 					'url' => \Elementor\Utils::get_placeholder_image_src(),
@@ -54,7 +61,7 @@ class Memory_Widget extends \Elementor\Widget_Base {
 		$this->add_control(
 			'face',
 			[
-				'label' => esc_html__( 'Choose Image', 'textdomain' ),
+				'label' => esc_html__( 'face caché', 'textdomain' ),
 				'type' => \Elementor\Controls_Manager::MEDIA,
 				'default' => [
 					'url' => \Elementor\Utils::get_placeholder_image_src(),
@@ -64,37 +71,70 @@ class Memory_Widget extends \Elementor\Widget_Base {
 		$this->add_control(
 			'win',
 			[
-				'label' => esc_html__( 'Choose Image', 'textdomain' ),
+				'label' => esc_html__( 'face gagné!', 'textdomain' ),
 				'type' => \Elementor\Controls_Manager::MEDIA,
 				'default' => [
 					'url' => \Elementor\Utils::get_placeholder_image_src(),
 				],
 			]
 		);
+		$this->add_control(
+			'msg_win',
+			[
+				'label' => esc_html__( 'message lorsque le joueur gagne', 'textdomain' ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'placeholder' => esc_html__( 'entrez du texte', 'textdomain' ),
+			]
+		);
+		$this->add_control(
+			'msg_replay',
+			[
+				'label' => esc_html__( 'msg sur le bouton pour rejouer', 'textdomain' ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'placeholder' => esc_html__( 'Entrez du texte', 'textdomain' ),
+			]
+		);
+		
+		//fin de la section des controles
 		$this->end_controls_section();
-
-
-		// Style Tab End
 
 	}
  
+		//dépéndence scripts js
     	public function get_script_depends() {
 		return [ 'memory_widget_js' ];
 	}
+	    //dépédences fichier css
     	public function get_style_depends() {
 		return [ 'memory_widget_css' ];
 	}
+	//rendu html du widget
 	protected function render() {
 	
 		?>
 		<?php
+			//récupération des paramétres enregistrés
 			$settings = $this->get_settings_for_display();
+		//récupération des urls des images dans un tableau
 		$list = array_column($settings['list'],'images');
 		$list = array_column($list,'url');
+		 //création des doublons pour chaque image
 		 array_push($list, ...$list);
+		 //mélange du tableau
 		shuffle($list);
-							echo '<div id="win"><p class="legend">Bravo, vous avez gagné!</p> <button id="replay">rejouer</button></div>';
+		/*début du code html avec les variables
+		msg_win correspond au message affiché lorsque le joueur gagne ['msg_replay'] pour le bouton rejouer. 
+		note: cette section n'apparaît que lorsque le joueur gagne)
+		memorygame section globale dans laquelle se trouve le jeu en général.
+		['win']['url'] url des images lorsque l'utilisateur trouve les deux mêmes cartes
+		['face']['url'] lorsque les cartes sont faces cachés
+		*/
+							echo '<div id="win"><p class="legend">'.$settings['msg_win'].'</p> <button id="replay">'.$settings['msg_replay'].'</button></div>';
 		echo '<div id="memorygame" data-win="' . $settings['win']['url'] . '" data-face="' . $settings['face']['url'] . '">';
+		/*
+		boucle pour chaque image, on met l'image de carte retourné, son url avec l'image réele (data-realImage représente l'image réele relié
+		a la carte) data-IsShowed est un paramétre permettant de savoir si l'image est face caché ou non.
+		 */
 		foreach ($list as $index => $item):
 			echo '<div class="divBack"><button class="memory-button"><img src="'.$settings['face']['url'].'" data-realImage="'.$item.
 			'" data-isShowed="false"></img></button></div>';
